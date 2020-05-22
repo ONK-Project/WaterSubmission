2bfb355ed6f1ac89e32d8ecaffb3458c696e64e1
andreas <au561064@post.au.dk>
2020-05-22 15:35:32 +0200

    Added mq instead of http

---
+++ WaterSubmission/Data/KubeMQSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WaterSubmission.Data
+{
+    public interface IAccountControlKubeMQSettings
+    {
+        string ChannelName { get; set; }
+        string ClientID { get; set; }
+        string KubeMQServerAddress { get; set; }
+    }
+    public class AccountControlKubeMQSettings : IAccountControlKubeMQSettings
+    {
+        public string ChannelName { get; set; }
+        public string ClientID { get; set; }
+        public string KubeMQServerAddress { get; set; }
+    }
+}
